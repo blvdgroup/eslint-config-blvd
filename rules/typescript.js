68bb7ca7cba22340223ae84ecbe28aad83de9a37
Resi Respati <resir014@gmail.com>
2019-07-07 16:17:40 +0700

    feat: upgrade deps + improve rules (#1)

--- rules/typescript.js
+++ rules/typescript.js
@@ -3,12 +3,19 @@
 // Reference https://github.com/typescript-eslint/typescript-eslint
 
 module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended'],
-  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/eslint-recommended'],
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true
+      },
+      plugins: ['@typescript-eslint'],
       rules: {
         'no-undef': 'off',
         'no-unused-vars': 'off'
@@ -21,9 +28,5 @@ module.exports = {
         extensions: ['.js', '.jsx', '.ts', '.tsx']
       }
     }
-  },
-  rules: {
-    // Disable this rule for typescript.
-    'no-undef': 'off'
   }
 }
